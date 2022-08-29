294702bc0a60bb29f75694373da5d3817ca64841
fmchu1181 <41478143+fmchu1181@users.noreply.github.com>
2022-08-30 05:33:17 +0800

    Add files via upload

--- camera.py
+++ camera.py
@@ -1,15 +1,17 @@
+#拍照片
 import cv2
+
 # 設定從哪顆鏡頭讀取影像，在括弧中填入先前查詢到的webcam編號
-
 webcam = cv2.VideoCapture(0)
+
 # 讀取影像
-
 return_value, image = webcam.read()
-# 儲存名為Picture.png的照片
 
+# 儲存名為Picture.png的照片
 cv2.imwrite("Picture.png", image)
 # 刪除webcam，避免影像佔用資源
-cv2.imshow('out',image)
 del(webcam)
+#show出剛剛拍的照片
+cv2.imshow('out',image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ run.py
@@ -0,0 +1,111 @@
+#基於黑線偏移量值開始尋機
+import RPi.GPIO as gpio
+#import time
+import cv2
+import numpy as np
+
+# 定义引脚
+gpio.setmode(gpio.BOARD)
+pin1 = 35
+pin2 = 36
+pin3 = 37
+pin4 = 38
+
+gpio.setup(pin1, gpio.OUT)
+gpio.setup(pin2, gpio.OUT)
+gpio.setup(pin3, gpio.OUT)
+gpio.setup(pin4, gpio.OUT)
+
+center=320
+#call攝影機出來拍照
+vp= cv2.VideoCapture(0)
+return_value, img= vp.read()
+cv2.imwrite("1.png", img)
+#灰階
+#cv2.imread("1.png",img)
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 反二值化
+ret,BW= cv2.threshold(img_gray, 180, 255, cv2.THRESH_BINARY_INV) 
+cv2.imwrite("BW.png",BW)
+
+# 单看第400行的像素值
+color =BW[400] 
+# 找到黑色的像素点个数
+white_count = np.sum(color == 0)
+# 找到黑色的像素点索引
+white_index = np.where(color == 0)
+
+# 防止white_count=0的报错
+if white_count == 0:
+    white_count = 1
+
+# 找到白色像素的中心点位置
+center = (white_index[0][white_count - 1] + white_index[0][0]) / 2
+
+# 计算出center与标准中心点的偏移量（圖片預設像素為480*640）X軸為640
+direction = center - 320
+vp= cv2.VideoCapture(0)
+while(1):
+    #call攝影機出來拍照
+    vp= cv2.VideoCapture(0)
+    return_value, img= vp.read()
+    cv2.imwrite("1.png", img)
+    #灰階
+    #cv2.imread("1.png",img)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 反二值化
+    ret,BW= cv2.threshold(img_gray, 180, 255, cv2.THRESH_BINARY_INV) 
+    cv2.imwrite("BW.png",BW)
+
+    # 单看第400行的像素值
+    color =BW[400] 
+    # 找到黑色的像素点个数
+    white_count = np.sum(color == 0)
+    # 找到黑色的像素点索引
+    white_index = np.where(color == 0)
+
+    # 防止white_count=0的报错
+    if white_count == 0:
+        white_count = 1
+
+    # 找到白色像素的中心点位置
+    center = (white_index[0][white_count - 1] + white_index[0][0]) / 2
+
+    # 计算出center与标准中心点的偏移量（圖片預設像素為480*640）X軸為640
+    direction = center - 320
+    print(direction)                            #偏移量值
+
+    # 停止
+    if abs(direction) > 250:
+            gpio.output(pin1, False)
+            gpio.output(pin2, False)
+            gpio.output(pin3, False)
+            gpio.output(pin4, False)
+
+    # 右转
+    elif direction >= 0:
+        # 限制在70以内
+        if direction > 70:
+            direction = 70
+        gpio.output(pin1, False)
+        gpio.output(pin2, True)
+        gpio.output(pin3, False)
+        gpio.output(pin4, False)
+
+    # 左转
+    elif direction < 0:
+        if direction < -70:
+            direction = -70
+        gpio.output(pin1, False)
+        gpio.output(pin2, False)
+        gpio.output(pin3, False)
+        gpio.output(pin4, True)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+# 释放清理
+vp.release()
+cv2.destroyAllWindows()
+gpio.cleanup()
